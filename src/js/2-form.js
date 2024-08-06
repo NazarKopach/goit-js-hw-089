4c10b98bfb9092494c0fe6a5d8fe0751f4b26683
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2024-08-06 12:03:43 +0200

    del err

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,14 +1,24 @@
 const feedbackFormEl = document.querySelector('.feedback-form');
 const formData = {};
 
+const fillFormField = () => {
+    const formdDataFromLs = JSON.parse(localStorage.getItem('feedback-form-data'));
+
+    if (formdDataFromLs === null) {
+        return;
+    }
+    console.log(formdDataFromLs);
+}
+
+fillFormField();
+
 const onFormFieldInput = event => {
     const fieldName = event.target.name;
     const fieldValue = event.target.value;
-    console.log(`${fieldName}: ${fieldValue}`);
-    formData[fieldName] = fieldValue;
-    console.log(formData);
 
-    localStorage.setItem('feedback-form-data', formData)
+    formData[fieldName] = fieldValue;
+
+    localStorage.setItem('feedback-form-data', JSON.stringify(formData));
 }
 
 feedbackFormEl.addEventListener('input', onFormFieldInput)
