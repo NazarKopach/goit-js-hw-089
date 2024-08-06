ada159d7324bc8bc6aa13ca2980daa3076f5d0fe
NazarKopach <164062094+NazarKopach@users.noreply.github.com>
2024-08-06 09:12:21 +0000

    Deploying to gh-pages from @ NazarKopach/goit-js-hw-09@a8d4a80b8aa19645bbf9b003e53fba82b68ee7b3 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/1-gallery-15aa6a01.js";/* empty css                      */import"./assets/vendor-10cb7c31.js";const r=document.querySelector(".feedback-form"),e={},c=o=>{const t=o.target.name,a=o.target.value;console.log(`${t}: ${a}`),e[t]=a,console.log(e),localStorage.setItem("feedback-form-data",e)};r.addEventListener("input",c);
+//# sourceMappingURL=commonHelpers3.js.map
